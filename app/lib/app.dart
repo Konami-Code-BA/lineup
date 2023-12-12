84e5f18e2e2124e5232d783c5d405aceacb52517
jzPLT <jeffzhaojapa@gmail.com>
2023-12-12 17:48:13 +0900

    Updated main app page to hold a list of routes instead of just a home page

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lineup/home.dart';
+import 'package:lineup/ui/pages/login_page.dart';
 
 class LineupApp extends StatelessWidget {
   const LineupApp({super.key});
@@ -13,7 +14,14 @@ class LineupApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
-      home: const LineupHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) {
+          return LoginPage();
+        },
+        '/': (context) => const LineupHomePage()
+      },
+      // home: const LineupHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
